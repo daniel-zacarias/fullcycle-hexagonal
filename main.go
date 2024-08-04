3a66eca6337d84dad425b971ee4aa482a450c886
Daniel Zacarias <daniel@DESKTOP-68C20A1>
2024-08-04 19:42:58 -0300

    Add cli implementation

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
 package main
 
-import (
-	"database/sql"
-
-	db2 "github.com/codeedu/go-hexagonal/adapters/db"
-	"github.com/codeedu/go-hexagonal/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/codeedu/go-hexagonal/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "db.sqlite")
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product Example", 10)
-	productService.Enable(product)
+	cmd.Execute()
 }
